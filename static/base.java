96fcf1cf5637aefbc54561570734926dee7453e7
Mitchell Nuckols <mitchnuckols@gmail.com>
2017-08-03 21:33:50 -0700

    Added install and setup section

---
+++ static/base.java
@@ -0,0 +1,5 @@
+package me.mitchell.helloworld;
+
+public class HelloWorld {
+
+}
--- static/helloworld.java
+++ static/helloworld.java
@@ -1,8 +1,8 @@
-public class HelloWorld {
+package me.mitchell.helloworld;
 
+public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
-
 }
